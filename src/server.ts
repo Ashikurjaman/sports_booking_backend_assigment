40bc5a51df26c87072091cf7fae93e6dfafadd58
Ashikurjaman <ashikbur007@gmail.com>
2024-05-18 00:16:33 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import app from "./app";
+import mongoose from "mongoose";
+import config from "./app/config";
+
+
+
+
+async function main() {
+
+  try {
+    await mongoose.connect(config.database_url as string);
+    const port = config.port;
+  
+  
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+      })
+  } catch (error) {
+    console.log(error)
+  }
+ 
+  
+}
+
+main();
+
+
+
+  
